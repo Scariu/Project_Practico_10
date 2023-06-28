d7537f90aaf60af9be3888e003b0baeb35853816
Flopy <f.hidalgoalarcon@gmail.com>
2023-06-27 20:23:31 -0400

    Se agrega funcion a botón CreateAccount y se agrega Toast.

--- app/src/main/java/com/example/project_practico_10/MainActivity.java
+++ app/src/main/java/com/example/project_practico_10/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.project_practico_10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.project_practico_10.databinding.ActivityMainBinding;
 
@@ -17,7 +19,16 @@ private ActivityMainBinding binding;
     }
 
     private void initListeners() {
-
-
+binding.buttonCreateAccount.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        String name = binding.editTextName.getText().toString();
+        String lastName = binding.editTextLastName.getText().toString();
+        String email = binding.editTextEmail.getText().toString();
+        String pass = binding.editTextPassword.getText().toString();
+        String text = "Usuario: " + name + " " + lastName + " " + "Email: " + email + " " + "Contraseña: " + pass;
+        Toast.makeText(getBaseContext(), text, Toast.LENGTH_LONG).show();
+    }
+});
     }
 }
